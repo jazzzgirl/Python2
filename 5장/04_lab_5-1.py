99f390d3138f08e8bdfa9177712012a8549edd2c
KIMJY <122241455+jazzzgirl@users.noreply.github.com>
2023-10-04 11:50:52 +0900

    Add files via upload

---
+++ 5장/04_lab_5-1.py
@@ -0,0 +1,16 @@
+'''
+    작성일 : 2023년 9월 27일
+    작성자 : 학과 학번 이름
+    설명 :  터틀 그래픽으로 여러 개의 원을 그려보자 
+'''
+import turtle as t 
+
+# 원 하나 그리기
+#t.circle(100)
+
+for count in range(10) : 
+    t.circle(100)
+    t.left(360 / 10)
+
+t.mainloop()   # 종료
+#t.done() # 종료
